a7bcd1570a1e58d5b2a4732399621655ce6a1c3c
Milanrdnvc <milanradinovic@rocketmail.com>
2020-08-06 18:23:01 +0200

    Altered gsap animations and made a functional mobile nav

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,13 @@ const gridBtns = document.querySelectorAll('.projects__grid-item-wrapper .btn');
 const header = document.querySelector('header');
 const homeText = document.querySelectorAll('.home__text');
 const firstPara = document.querySelector('.para');
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('translate-zero');
+    nav.style.transition = 'all .5s ease';
+});
 
 gridItemWrapper.forEach((item, index) => {
     item.addEventListener('click', () => {
@@ -23,6 +30,7 @@ gridItemWrapper.forEach((item, index) => {
                 gridBtns[4].classList.toggle('grid-btn-toggle');
                 gridBtns[5].classList.toggle('grid-btn-toggle');
                 item.classList.toggle('grid-item-wrapper-toggle');
+                break;
         }
     });
 });
@@ -30,8 +38,19 @@ gridItemWrapper.forEach((item, index) => {
 const events = ['scroll', 'load'];
 for(let event of events){
     window.addEventListener(event, () => {
-        this.scrollY > 50? header.style.backgroundColor = 'rgba(0, 0, 0, .5)' : header.style.backgroundColor = 'transparent';
-        header.style.transition = 'all 0.3s ease'
+        // this.scrollY > 50? header.style.backgroundColor = 'rgba(0, 0, 0, .5)' : header.style.backgroundColor = 'transparent';
+        if(this.scrollY > 50){
+            header.style.backgroundColor = 'rgba(0, 0, 0, .5)';
+            if(window.clientWidth < 900){
+                console.log('hello')
+            }
+            nav.style.backgroundColor = 'rgba(0, 0, 0, .5)';
+        } else{
+            header.style.backgroundColor = 'transparent';
+            nav.style.backgroundColor = 'transparent';
+        }
+        header.style.transition = 'background-color 0.3s ease';
+        nav.style.transition = 'background-color 0.3s ease';
     });
 }
 
@@ -43,12 +62,7 @@ timeline
     .from('.home__social', {y: '250%', opacity: 0, ease: 'power4'}, 0);
 
 gsap.registerPlugin(ScrollTrigger);
-gsap.from(firstPara, {scrollTrigger: {trigger: firstPara, toggleActions: 'restart reverse restart reverse', start: 'top 70%', end: 'top 10%'}, y: '30%', opacity: 0, ease: 'power4', duration: 1});
-gsap.from('.services__skill', {scrollTrigger: {trigger: '.services__skill', toggleActions: 'restart reverse restart reverse', start: 'top 90%', end: 'top 10%'}, y: '30%', opacity: 0, ease: 'power4', duration: 1, stagger: .05});
-
-// gsap.from(para[0], {scrollTrigger: {trigger: para[0], toggleActions: 'restart none none none'}, y: '150%', opacity: 0, ease: 'power4', duration: 1});
-// gsap.from(para[1], {scrollTrigger: {trigger: para[1], toggleActions: 'restart none none none'}, y: '150%', opacity: 0, ease: 'power4', duration: 1, delay: 1});
-// gsap.from(para[2], {scrollTrigger: {trigger: para[2], toggleActions: 'restart none none none'}, y: '150%', opacity: 0, ease: 'power4', duration: 1, delay: 2});
-// gsap.from(para[3], {scrollTrigger: {trigger: para[3], toggleActions: 'restart none none none'}, y: '150%', opacity: 0, ease: 'power4', duration: 1, delay: 3});
+gsap.from(firstPara,{scrollTrigger:{trigger: firstPara, start: 'top 70%', end: 'top 10%'}, y: '30%', opacity: 0, ease: 'power4', duration: 1});
+gsap.from('.services__skill', {scrollTrigger: {trigger: '.services__skill', start: 'top 90%', end: 'top 10%'}, y: '30%', opacity: 0, ease: 'power4', duration: 1, stagger: .05});
 
 
